3cb75aa5a3f5eb72e506759378a0f42c6dc97c43
Tsaone <tsaoneb3@gmail.com>
2023-09-03 09:34:29 +0200

    started on the JS of the app

---
+++ script.js
@@ -0,0 +1,8 @@
+let equal_pressed = 0;
+
+let button_input = document.querySelectorAll(".input-button");
+
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
