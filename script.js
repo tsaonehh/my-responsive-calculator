41810732536f6baee48804af0a86dd9ad5e65355
Tsaone <tsaoneb3@gmail.com>
2023-09-05 19:58:08 +0200

    Made changes to the JS file

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const dislpay = document.querySelector(".display");
+const display = document.querySelector(".display");
 const buttons = document.querySelector("button");
 const specialCharacters = ["%", "*", "/", "-", "+", "="];
 let output = "";
+
+const calculate = (btnValue) => {
+    if (btnValue === "=" && btnValue !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC"){
+        output = "";
+    } else if (btnValue === "DEL"){
+        output = output.toString().slice(0, -1);
+    } else {
+    }
+    display.value = output;
+};
 
 let input = document.getElementById("input");
 let equal = document.getElementById("equal");
